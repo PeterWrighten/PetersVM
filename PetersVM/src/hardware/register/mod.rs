24bebaed5b07b9ea24009e0d1f5f21b16925ddb1
PeterWrighten <peterwrighten@gmail.com>
2022-02-20 00:32:40 +0900

    init

---
+++ PetersVM/src/hardware/register/mod.rs
@@ -0,0 +1,29 @@
+pub struct Registers {
+    pub r0: u16,
+    pub r1: u16,
+    pub r2: u16,
+    pub r3: u16,
+    pub r4: u16,
+    pub r5: u16,
+    pub r6: u16,
+    pub r7: u16,
+    pub pc: u16,
+    pub cond: u16,
+}
+
+impl Registers {
+    pub fn new() -> Registers {
+        Registers {
+            r0: 0,
+            r1: 0,
+            r2: 0,
+            r3: 0,
+            r4: 0,
+            r5: 0,
+            r6: 0,
+            r7: 0,
+            pc: PC_START,
+            cond: 0,
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Peters Virtual Machine
+
+My Virtual Machine almost based on LC3 implemented with pure Rust.
+
+# Reference
+
+[LC3 Doc](https://github.com/justinmeiners/lc3-vm)
+
+[Rustlang](https://www.rodrigoaraujo.me/posts/lets-build-an-lc-3-virtual-machine/)
